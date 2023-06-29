8a6387fbdc06005c39c250d3aaf88e57caac9aee
emre1n <emre1n@protonmail.com>
2023-06-29 16:02:25 +0300

    setup initial express server for backend api

---
+++ client/src/components/Meals/AvailableMeals/index.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import styles from './styles.module.css';
+
+import Card from '../../UI/Card';
+import MealItem from '../MealItem';
+import IMeals from '../../../libs/models/meals.model';
+
+const AvailableMeals = () => {
+  const [meals, setMeals] = useState<IMeals[]>([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch('/api');
+      const responseData = await response.json();
+      setMeals(responseData.meals);
+    };
+
+    fetchMeals();
+  }, []);
+
+  const mealsList = meals.map(meal => (
+    <MealItem
+      id={meal.id}
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={styles.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
