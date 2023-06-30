17cee8f9520dc4fd525a24554e047cdf2393e0c5
emre1n <emre1n@protonmail.com>
2023-06-30 15:46:07 +0300

    add loading state

--- client/src/components/Meals/AvailableMeals/index.tsx
+++ client/src/components/Meals/AvailableMeals/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './styles.module.css';
 
 import Card from '../../UI/Card';
@@ -7,16 +7,26 @@ import IMeals from '../../../libs/models/meals.model';
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState<IMeals[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch('/api/meals');
       const responseData = await response.json();
       setMeals(responseData);
+      setIsLoading(false);
     };
 
     fetchMeals();
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={styles.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
 
   const mealsList = meals.map(meal => (
     <MealItem
